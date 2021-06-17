0c601ed7333914b729ebc9cd424494f48024499d
jonaslilja <jonaslilja13@gmail.com>
2021-06-17 11:04:26 +0200

    init Python repo

---
+++ hello.py
@@ -0,0 +1,22 @@
+Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> spam = 42 + 42
+>>> spam
+84
+>>> 
+============================================================================== RESTART: /Users/jonaslilja/Documents/name.py ==============================================================================
+Write your name: 
+Write your name:Jonas
+your name isWrite your name:Jonas
+>>> 
+============================================================================== RESTART: /Users/jonaslilja/Documents/name.py ==============================================================================
+Write your name: 
+Write your name:jonas
+your name isWrite your name:jonas
+>>> 
+============================================================================== RESTART: /Users/jonaslilja/Documents/name.py ==============================================================================
+Write your name: 
+jonas
+your name isjonas
+>>> 
